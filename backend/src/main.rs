692ef19530cc48651629ce1632e8b8e8c2bcf0e0
Lillian Armes <lilyarmes@protonmail.com>
2022-01-04 05:22:35 -0500

    :construction: Actix web server -w-

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Meow meow, world!");
+use actix_web::{get, App, HttpServer, Responder};
+use dotenv::dotenv;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Meow!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
